b42633c3ddc3cb1f41a69ab3f0cfd2e66fef5389
Кирилл Петухов <test@mail.ru>
2025-02-15 15:22:32 +0300

    docker

--- back/app/celery/llm.py
+++ back/app/celery/llm.py
@@ -3,43 +3,35 @@ import warnings
 warnings.filterwarnings('ignore')
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
-from app.celery.prompt import SLIDE_PROMPT, SLIDE_UPDATE, TITLE_PROMPT
+from prompt import SLIDE_PROMPT, SLIDE_UPDATE, TITLE_PROMPT, SUMMARIZE_PROMPT
 from pydantic import BaseModel, Field
 
 
-llm = ChatOllama(model="qwen2.5:14b", temperature=0.0, base_url=f"http://91.236.197.228:11434")
-
-
-def get_presentation_content(theme, num_slides = 5):
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", TITLE_PROMPT),
-            ("user", "{theme}, {num_slides}"),
-        ]
-    )
-    messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
-    response = llm.invoke(messages)
-    answer = response.content
-
-    return answer
+llm = ChatOllama(model="qwen2.5:14b", temperature=0.0)
 
 def get_slide_2(theme, header, history = ""):
     return "agsdhjhagskdhakshdk PROMT"
 
-def get_slide(theme, header, history = ""):
+def get_presentation_content_structured_2(theme, num_slides = 5):
+    return {'slides': {'slide_1': 'Введение в Росатом',
+                'slide_2': 'История и развитие Росатома',
+                'slide_3': 'Основные направления деятельности Росатома',
+                'slide_4': 'Производственные мощности и инфраструктура',
+                'slide_5': 'Международное сотрудничество и экологические стандарты'}}
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", SLIDE_PROMPT),
-            ("user", "{theme}, {header}, {num_slides}"),
-        ]
-    )
-    messages = prompt.invoke({"theme": theme, "header": header, "history": history})
-    response = llm.invoke(messages)
-    answer = response.content
+# def get_presentation_content(theme, num_slides = 5):
 
-    return answer
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ("system", TITLE_PROMPT),
+#             ("user", "{theme}, {num_slides}"),
+#         ]
+#     )
+#     messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
+#     response = llm.invoke(messages)
+#     answer = response.content
+
+#     return answer
 
 
 class Slide(BaseModel):
@@ -51,26 +43,47 @@ class SlidesList(BaseModel):
     slides: dict[str, str] = Field(description="Словарь слайдов")
 
 
-def get_presentation_content_structured_2(theme, num_slides = 5):
-    return {'slides': {'slide_1': 'Введение в Росатом',
-                'slide_2': 'История и развитие Росатома',
-                'slide_3': 'Основные направления деятельности Росатома',
-                'slide_4': 'Производственные мощности и инфраструктура',
-                'slide_5': 'Международное сотрудничество и экологические стандарты'}}
-
-def get_presentation_content_structured(theme, num_slides = 5):
+def get_presentation_content_structured(theme, num_slides = 5, content=""):
 
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", TITLE_PROMPT),
-            ("user", "{theme}, {num_slides}"),
+            ("user", "{theme}, {num_slides}, {content}"),
         ]
     )
-    messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
+    messages = prompt.invoke({"theme": theme, "num_slides": num_slides, "content": content})
     model_with_structure = llm.with_structured_output(SlidesList)
     structured_output = model_with_structure.invoke(messages)
     
     return structured_output.model_dump()
+
+
+def get_summary(context, num_slides=5):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SUMMARIZE_PROMPT),
+            ("user", "{num_slides}, {context}"),
+        ]
+    )
+    messages = prompt.invoke({"num_slides": num_slides, "context": context})
+    response = llm.invoke(messages)
+    answer = response.content
+    return answer
+
+
+def get_slide(theme, header, history = "", context = ""):
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SLIDE_PROMPT),
+            ("user", "{theme}, {header}, {history}, {context}"),
+        ]
+    )
+    messages = prompt.invoke({"theme": theme, "header": header, "history": history, "context": context})
+    response = llm.invoke(messages)
+    answer = response.content
+
+    return answer
 
 
 def update_slide(theme, header, text, added_text = ""):
---
+++ back/app/celery/rag.py
@@ -0,0 +1,42 @@
+import warnings
+warnings.filterwarnings('ignore')
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
+from langchain_unstructured import UnstructuredLoader
+
+
+# from langchain_ollama import OllamaEmbeddings
+# from langchain_core.vectorstores import InMemoryVectorStore
+# llm = ChatOllama(model="qwen2.5:14b", temperature=0.05)
+# embeddings = OllamaEmbeddings(model="bge-m3")
+# vector_store = InMemoryVectorStore(embeddings)
+
+
+def parse_file_in_document(file):
+    loader = UnstructuredLoader(file)
+    document = loader.load()
+    return document
+
+
+def get_text_from_document(document):
+    text = ''
+    for item in document:
+        text += item.page_content
+    text = text.replace('\ufffe', "").replace('\r\n', " ")
+    return text
+
+
+def create_vector_store(vector_store, document):
+    r_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=4048,
+        chunk_overlap=256,
+        separators=["\n\n", "\n", "(?<=\. )", " ", ""]
+    )
+    chunks = r_splitter.split_documents(document)
+    _ = vector_store.add_documents(documents=chunks)
+    return vector_store
+
+
+def get_rag_context(vector_store, slide_header):
+    retrieved_docs = vector_store.similarity_search(slide_header)
+    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+    return docs_content
