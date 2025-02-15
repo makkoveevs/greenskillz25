9b9e7e7ee86528d9ac707c6eb5990e747170f7a9
Vladislav Ershov <75930878+ErshovVE@users.noreply.github.com>
2025-02-15 14:22:33 +0300

    add rag

--- ml/llm.py
+++ ml/llm.py
@@ -3,41 +3,26 @@ import warnings
 warnings.filterwarnings('ignore')
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
-from prompt import SLIDE_PROMPT, SLIDE_UPDATE, TITLE_PROMPT
+from prompt import SLIDE_PROMPT, SLIDE_UPDATE, TITLE_PROMPT, SUMMARIZE_PROMPT
 from pydantic import BaseModel, Field
 
 
-llm = ChatOllama(model="qwen2.5:14b", temperature=0.0, base_url=f"http://91.236.197.228:11434")
+llm = ChatOllama(model="qwen2.5:14b", temperature=0.0)
 
 
-def get_presentation_content(theme, num_slides = 5):
+# def get_presentation_content(theme, num_slides = 5):
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", TITLE_PROMPT),
-            ("user", "{theme}, {num_slides}"),
-        ]
-    )
-    messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
-    response = llm.invoke(messages)
-    answer = response.content
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             ("system", TITLE_PROMPT),
+#             ("user", "{theme}, {num_slides}"),
+#         ]
+#     )
+#     messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
+#     response = llm.invoke(messages)
+#     answer = response.content
 
-    return answer
-
-
-def get_slide(theme, header, history = ""):
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", SLIDE_PROMPT),
-            ("user", "{theme}, {header}, {num_slides}"),
-        ]
-    )
-    messages = prompt.invoke({"theme": theme, "header": header, "history": history})
-    response = llm.invoke(messages)
-    answer = response.content
-
-    return answer
+#     return answer
 
 
 class Slide(BaseModel):
@@ -49,19 +34,47 @@ class SlidesList(BaseModel):
     slides: dict[str, str] = Field(description="Словарь слайдов")
 
 
-def get_presentation_content_structured(theme, num_slides = 5):
+def get_presentation_content_structured(theme, num_slides = 5, content=""):
 
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", TITLE_PROMPT),
-            ("user", "{theme}, {num_slides}"),
+            ("user", "{theme}, {num_slides}, {content}"),
         ]
     )
-    messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
+    messages = prompt.invoke({"theme": theme, "num_slides": num_slides, "content": content})
     model_with_structure = llm.with_structured_output(SlidesList)
     structured_output = model_with_structure.invoke(messages)
     
     return structured_output.model_dump()
+
+
+def get_summary(context, num_slides=5):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SUMMARIZE_PROMPT),
+            ("user", "{num_slides}, {context}"),
+        ]
+    )
+    messages = prompt.invoke({"num_slides": num_slides, "context": context})
+    response = llm.invoke(messages)
+    answer = response.content
+    return answer
+
+
+def get_slide(theme, header, history = "", context = ""):
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SLIDE_PROMPT),
+            ("user", "{theme}, {header}, {history}, {context}"),
+        ]
+    )
+    messages = prompt.invoke({"theme": theme, "header": header, "history": history, "context": context})
+    response = llm.invoke(messages)
+    answer = response.content
+
+    return answer
 
 
 def update_slide(theme, header, text, added_text = ""):
