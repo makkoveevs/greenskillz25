1177d28cf0d7187e8181534162771c5e097cc1ed
Vladislav Ershov <75930878+ErshovVE@users.noreply.github.com>
2025-02-15 11:57:26 +0300

    upd ml

--- ml/llm.py
+++ ml/llm.py
@@ -1,11 +1,13 @@
 import warnings
+
 warnings.filterwarnings('ignore')
-from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
-from prompt import TITLE_PROMPT, SLIDE_PROMPT, SLIDE_REGENERATE
+from langchain_ollama import ChatOllama
+from prompt import SLIDE_PROMPT, SLIDE_UPDATE, TITLE_PROMPT
+from pydantic import BaseModel, Field
 
 
-llm = ChatOllama(model="qwen2.5:1.5b", temperature=0.0)
+llm = ChatOllama(model="qwen2.5:14b", temperature=0.0, base_url=f"http://91.236.197.228:11434")
 
 
 def get_presentation_content(theme, num_slides = 5):
@@ -32,6 +34,45 @@ def get_slide(theme, header, history = ""):
         ]
     )
     messages = prompt.invoke({"theme": theme, "header": header, "history": history})
+    response = llm.invoke(messages)
+    answer = response.content
+
+    return answer
+
+
+class Slide(BaseModel):
+    number: int = Field(description="Номер слайда")
+    header: str = Field(description="Заголовок слайда")
+
+
+class SlidesList(BaseModel):
+    slides: dict[str, str] = Field(description="Словарь слайдов")
+
+
+def get_presentation_content_structured(theme, num_slides = 5):
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", TITLE_PROMPT),
+            ("user", "{theme}, {num_slides}"),
+        ]
+    )
+    messages = prompt.invoke({"theme": theme, "num_slides": num_slides})
+    model_with_structure = llm.with_structured_output(SlidesList)
+    structured_output = model_with_structure.invoke(messages)
+    
+    return structured_output.model_dump()
+
+
+def update_slide(theme, header, text, added_text = ""):
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", SLIDE_UPDATE),
+            ("user", "{theme}, {header}, {text}, {added_text}"),
+        ]
+    )
+    messages = prompt.invoke({"theme": theme, "header": header, "text": text, "added_text": added_text})
     response = llm.invoke(messages)
     answer = response.content
 
--- ml/prompt.py
+++ ml/prompt.py
@@ -5,10 +5,11 @@ TITLE_PROMPT = (
     "Ensure logical flow between sections. Answer on Russian language."
     "Example: "
     "<<<USER_INPUT>>> theme - Солнечная система, slides number - 7 <<<USER_INPUT>>>"
-    "<<<AI_OUTPUT>>> ### Slide 1: Введение \n ### Slide 2: Состав Солнечной системы\n "
-    "### Slide 3: Солнце \n ### Slide 4: Планеты земной группы \n ### Slide 5: Пояс астероидов \n"
-    "### Slide 6: Газовые гиганты \n ### Slide 7: Плутон \n <<<AI_OUTPUT>>>"
+    "<<<AI_OUTPUT>>> ### Slide 1: Введение \n\n ### Slide 2: Состав Солнечной системы\n\n "
+    "### Slide 3: Солнце \n\n ### Slide 4: Планеты земной группы \n\n ### Slide 5: Пояс астероидов \n\n"
+    "### Slide 6: Газовые гиганты \n\n ### Slide 7: Плутон \n\n <<<AI_OUTPUT>>>"
 )
+
 
 SLIDE_PROMPT = (
     "You're an expert AI assistant in presentation. "
@@ -24,6 +25,18 @@ SLIDE_PROMPT = (
 )
 
 
-SLIDE_REGENERATE = (
-""
+SLIDE_UPDATE = (
+    "You're an expert AI assistant in presentation. "
+    "Here are presentation theme - {theme} and slide header - {header}. "
+    "Current main text - {text}. Added text - {added_text}. "
+    "Your task is to update or regenerate the current slide text using added text."
+    "Maintain professional tone. The text must be no more than 100 words. Answer on Russian language."
+    "Example: "
+    "<<<USER_INPUT>>> theme - Солнечная система, header - Планеты земной группы, "
+    "text - Планеты земной группы включают Меркурий, Венеру, Землю и Марс. "
+    "Они характеризуются малым размером и плотностью, с металлическим ядром и скальным слоем. "
+    "added_text - Единственная обитаемая планета - Земля. <<<USER_INPUT>>>"
+    "<<<AI_OUTPUT>>> Планеты земной группы включают Меркурий, Венеру, Землю и Марс. "
+    "Они характеризуются малым размером и плотностью, с металлическим ядром и скальным слоем. "
+    "Среди всех планет, единственная планета, на которой есть жизнь - Земля. <<<AI_OUTPUT>>>"
 )
