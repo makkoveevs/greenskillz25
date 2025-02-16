ad3ad9d02d3807fcc1b51d4c0f2b36cd0719cd3a
Кирилл Петухов <test@mail.ru>
2025-02-16 12:43:03 +0300

    update

--- back/app/api/endpoints/presentations.py
+++ back/app/api/endpoints/presentations.py
@@ -15,7 +15,8 @@ from app.schemas.auth_schemas import UserKeycloak
 from app.schemas.presentations_schema import (
     PresentationsRequestFile,
     PresentationsRequest, PresentationsRequestResponse,
-    PresentationsRequestResponseCompleted, PresentationsResultGet, Slide, PresentationsResultPatch
+    PresentationsRequestResponseCompleted, PresentationsResultGet, Slide, PresentationsResultPatch,
+    PresentationsResultSlideUpdate
 )
 from app.core.postgres import DBWork, Sort
 from app.models.models import (
@@ -259,3 +260,74 @@ async def download_presentation(
         media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation",
         headers={'Content-Disposition': 'attachment; filename=1.pptx',
                  "Content-Type": "application/vnd.openxmlformats-officedocument.presentationml.presentation"})
+
+
+@router.patch("/regex/{presentation_id}")
+async def update_presentation(
+        presentation_id: uuid.UUID,
+        presentation_data: PresentationsResultSlideUpdate,
+        # user: UserKeycloak = Depends(keycloak_client.get_current_user),
+        db_work: DBWork = Depends(get_db_work)
+):
+    presentation_obj: PresentationResultModel = await db_work.get_one_obj(
+        PresentationResultModel,
+        {'id': presentation_id}
+    )
+    # if not presentation_obj:
+    #     raise error_dict.get(ErrorName.DoesNotExist)
+    # if settings.DEFAULT_ADMIN_GROUP not in user.groups:
+    #     if str(presentation_obj.user_id) != str(user.sub):
+    #         raise error_dict.get(ErrorName.Forbidden)
+    slide = None
+    slide_header_block = {}
+    for i in presentation_data.slides:
+        if i.slide_number == presentation_data.slide_num:
+            slide_id = i.id
+            slide = i.elements
+            text_current = []
+            slide_header = ''
+            for j in slide:
+                if j.get('text_type', '') == 'regular':
+                    text_current.append(j.get('content', ''))
+                if j.get('text_type', '') == 'header':
+                    slide_header = j.get('content', '')
+                    slide_header_block = j
+            break
+    else:
+        return
+    text_current = ' '.join(text_current)
+    new_text = presentation_data.text
+    print(presentation_obj.theme)
+    print(slide_header)
+    print(text_current)
+    print(new_text)
+    content = update_slide(theme=presentation_obj.theme, header=slide_header, text=text_current, added_text=new_text)
+    if slide_header_block:
+        slide = [slide_header_block, {
+                "id": str(uuid.uuid4()),
+                "text_type": "regular",
+                "alignment": "left",
+                "style": "regular",
+                "size": 16,
+                "content": content,
+                "w": 0.8,
+                "h": 0.7,
+                "x": 0.2,
+                "y": 0.3,
+            }]
+    else:
+        slide = [
+            {
+                "id": str(uuid.uuid4()),
+                "text_type": "regular",
+                "alignment": "left",
+                "style": "regular",
+                "size": 16,
+                "content": content,
+                "w": 0.8,
+                "h": 0.7,
+                "x": 0.2,
+                "y": 0.3,
+            }
+        ]
+    await db_work.update_obj(SlideModel, [{'field': SlideModel.id, 'value': slide_id}], {"elements": slide})
