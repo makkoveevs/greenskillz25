fb06b2caefde575ba2fb5c93aa6e8ef995329b56
MakkoveevS <makkoveevs@mail.ru>
2025-02-16 10:32:30 +0300

    front prerelease

--- front/src/components/Tools/Tools.tsx
+++ front/src/components/Tools/Tools.tsx
@@ -1,8 +1,9 @@
-import { Button, Flex, Typography } from "antd";
+import { Button, Carousel, Flex, Modal, Typography } from "antd";
 import { ToolsStyled } from "./styles";
 import { observer } from "mobx-react-lite";
 import Model from "src/stores";
 import { useState } from "react";
+import { API_SOURCE } from "src/shared/constants";
 const { Title } = Typography;
 
 export const Tools = observer((): JSX.Element => {
@@ -16,13 +17,34 @@ export const Tools = observer((): JSX.Element => {
   } = Model;
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    handleExport();
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   const handleSave = (): void => {
     setIsLoading(true);
     savePresentation().finally(() => setIsLoading(false));
   };
   const handleExport = (): void => {
     setIsLoading(true);
-    exportPresentation().finally(() => setIsLoading(false));
+    exportPresentation(design).finally(() => {
+      setIsLoading(false);
+    });
+  };
+  const [design, setDesign] = useState<number>(1);
+  const onChange = (currentSlide: number) => {
+    setDesign(currentSlide);
   };
 
   return (
@@ -66,7 +88,7 @@ export const Tools = observer((): JSX.Element => {
               Сохранить
             </Button>
             <Button
-              onClick={handleExport}
+              onClick={showModal}
               disabled={isLoading}
               type="default"
               loading={isLoading}>
@@ -75,6 +97,35 @@ export const Tools = observer((): JSX.Element => {
           </Flex>
         </Flex>
       </Flex>
+      <Modal
+        title="Выбери тему"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}>
+        <Carousel
+          afterChange={onChange}
+          arrows={true}
+          dotPosition="left"
+          draggable={true}
+          dots={{ className: "dot" }}>
+          {new Array(7).fill(1).map((e, i) => (
+            <div
+              style={{
+                margin: 0,
+                color: "#fff",
+                lineHeight: "600px"
+              }}
+              key={e * i}>
+              <img
+                src={`${API_SOURCE}/${e * (i + 1)}.png`}
+                width={470}
+                height={300}
+                alt={`${e * (i + 1)}`}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </Modal>
     </ToolsStyled>
   );
 });
